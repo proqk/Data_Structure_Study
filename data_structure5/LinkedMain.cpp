e1d1cb5ccddada24ec9d85d081471811b32c89ce
Yujin Lee <proqk@naver.com>
2018-05-08 09:51:42 +0900

    Create LinkedMain.cpp

---
+++ data_structure5/LinkedMain.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "LinkedStack.h"
+
+int main(){
+	Stack s1;
+	Init(&s1);
+	for (int i = 1; i <= 5; i++){
+		printf("Push(%d)\n", i);
+		Push(&s1, i);
+		Display(&s1);
+	}
+
+	printf("Pop() : %d\n", Pop(&s1));
+	Display(&s1);
+
+	printf("Pop() : %d\n", Pop(&s1));
+	Display(&s1);
+
+	printf("Peek() : %d\n", Peek(&s1));
+	Display(&s1);
+
+	printf("\n\n");
+	return 0;
+}
