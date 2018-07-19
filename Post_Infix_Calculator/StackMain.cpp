045b341e67a9b38c5544861c3c1cdfda6460e307
Yujin Lee <proqk@naver.com>
2018-07-19 23:38:02 +0900

    For Calculator

---
+++ Post_Infix_Calculator/StackMain.cpp
@@ -0,0 +1,25 @@
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include "ArrayStack.h"
+//
+//int main() {
+//	Stack s1;
+//	Init(&s1);
+//	Push(&s1,'a');
+//	Push(&s1, 'b');
+//	Push(&s1, 'c');
+//	Push(&s1, 'd');
+//
+//	Display(&s1);
+//
+//	printf("Pop() : %c\n", Pop(&s1));
+//	Display(&s1);
+//
+//	printf("Pop() : %c\n", Pop(&s1));
+//	Display(&s1);
+//
+//	printf("Peek() : %c\n", Peek(&s1));
+//	Display(&s1);
+//
+//	printf("\n\n");
+//}
